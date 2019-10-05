1a776e36645390bbd5da5161cba04cf59cd051d6
Fernando <feh.wilinando@gmail.com>
2019-10-05 11:54:21 -0300

    Implemented circuit breaker

--- search/src/main/java/me/fwfurtado/SearchApplication.java
+++ search/src/main/java/me/fwfurtado/SearchApplication.java
@@ -2,8 +2,10 @@ package me.fwfurtado;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SearchApplication {
--- search/src/main/java/me/fwfurtado/SearchController.java
+++ search/src/main/java/me/fwfurtado/SearchController.java
@@ -1,8 +1,10 @@
 package me.fwfurtado;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +16,21 @@ class SearchController {
         this.profile = profile;
     }
 
+    enum Type {
+        ERROR, DEFAULT;
+    }
+
     @GetMapping
-    Map<String, String> show() {
+    @HystrixCommand(fallbackMethod = "fallback")
+    Map<String, String> show(@RequestParam(defaultValue = "DEFAULT") Type type) {
+
+        if (type == Type.ERROR) throw new IllegalStateException("Type error received");
+
         return Map.of("instance", profile);
+    }
+
+    Map<String, String> fallback(Type type) {
+        return Map.of("instance", type.name());
     }
 
 }
