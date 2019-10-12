799d06d99ad5e046e7c963f454fd47081fcb8190
Fernando <feh.wilinando@gmail.com>
2019-10-12 11:40:21 -0300

    add new config

--- search/src/main/java/me/fwfurtado/SearchController.java
+++ search/src/main/java/me/fwfurtado/SearchController.java
@@ -3,21 +3,30 @@ package me.fwfurtado;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 class SearchController {
 
     private final String profile;
+    private final String greeting;
 
-    SearchController(@Value("${spring.profiles.active:default}") String profile) {
+    SearchController(@Value("${spring.profiles.active:default}") String profile, @Value("${search.greeting:''}") String greeting) {
         this.profile = profile;
+        this.greeting = greeting;
     }
 
     enum Type {
         ERROR, DEFAULT;
+    }
+
+    @GetMapping("greet")
+    Map<String, String> refresh() {
+        return Map.of("search.greeting", greeting);
     }
 
     @GetMapping
